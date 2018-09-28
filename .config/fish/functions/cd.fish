37bc06104a9706ecc3206f7a9ee58d31f5a0e4fe
userWayneCampbell <wcampbell1995@gmail.com>
2018-09-28 11:16:33 -0400

    show dotfiles

--- .config/fish/functions/cd.fish
+++ .config/fish/functions/cd.fish
@@ -2,9 +2,9 @@
 function cd --description "auto ls for each cd"
   if [ -n $argv[1] ]
     builtin cd $argv[1]
-    and ls
+    and ls -A
   else
     builtin cd ~
-    and ls
+    and ls -A
   end
 end
