b244f3f1d4a7f9acd03f8dfe04b70ab2ead677ca
userWayneCampbell <wcampbell1995@gmail.com>
2018-10-31 16:30:27 -0400

    Move setting up monitor settings to fish function

--- fish/.config/fish/functions/lf.fish
+++ fish/.config/fish/functions/lf.fish
@@ -1,4 +1,5 @@
 # list custom functions
 function lf --description "list fish custom functions"
+    echo "Use type 'function' to display function information"
     ls ~/.config/fish/functions/ | sed -e 's/\.[^.]*$//'
 end
