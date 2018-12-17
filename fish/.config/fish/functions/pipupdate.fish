ca9afc69e41203201dd15f9f88ecc28bf2857981
userWayneCampbell <wcampbell1995@gmail.com>
2018-12-17 18:15:34 -0500

    Fix pipupdate

--- fish/.config/fish/functions/pipupdate.fish
+++ fish/.config/fish/functions/pipupdate.fish
@@ -1,4 +1,4 @@
 # Automatic pip list update
-function -cd --description "automatic pip list update"
+function pipupdate --description "automatic pip list update"
   pip list --outdated | sed -n '1,2!p' | awk '{system("pip install "$1" --upgrade --user")}'
 end
