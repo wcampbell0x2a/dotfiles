5e37e96bad6812a033f26ff5da78176f27afbf19
userWayneCampbell <wcampbell1995@gmail.com>
2018-12-17 18:15:05 -0500

    Improve fish functions/scripts display

--- fish/.config/fish/functions/lf.fish
+++ fish/.config/fish/functions/lf.fish
@@ -1,5 +1,13 @@
-# list custom functions
-function lf --description "list fish custom functions"
-    echo "Use type 'function' to display function information"
+# list custom functions / scripts
+function lf --description "list fish custom function / scripts"
+    echo "--------------"
+    echo "Fish Functions"
+    echo "-Use >type 'function name' to display function information"
+    echo "--------------"
     ls ~/.config/fish/functions/ | sed -e 's/\.[^.]*$//'
+    echo
+    echo "-------"
+    echo "Scripts"
+    echo "-------"
+    ls ~/scripts
 end
