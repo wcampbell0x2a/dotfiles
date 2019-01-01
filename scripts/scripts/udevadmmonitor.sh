203806af5d11b2f0c42c24a754ee7553eb9da97e
userWayneCampbell <wcampbell1995@gmail.com>
2019-01-01 12:18:06 -0500

    Add auto mount devices script
    Signed-off-by: userWayneCampbell <wcampbell1995@gmail.com>

---
+++ scripts/scripts/udevadmmonitor.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+##
+# Auto mount usb devices
+# 
+# found on: https://wiki.archlinux.org/index.php/udisks
+##
+
+pathtoname() {
+    udevadm info -p /sys/"$1" | awk -v FS== '/DEVNAME/ {print $2}'
+}
+
+stdbuf -oL -- udevadm monitor --udev -s block | while read -r -- _ _ event devpath _; do
+        if [ "$event" = add ]; then
+            devname=$(pathtoname "$devpath")
+            udisksctl mount --block-device "$devname" --no-user-interaction
+        fi
+done
