b6457956bb6a594504aff5b7467786c5ec655084
userWayneCampbell <wcampbell1995@gmail.com>
2019-01-01 16:21:19 -0500

    Improve battery script
    Improved the format of the output of the battery script. It now has a
    good header
    
    Signed-off-by: userWayneCampbell <wcampbell1995@gmail.com>

--- scripts/scripts/battery_notify.sh
+++ scripts/scripts/battery_notify.sh
@@ -11,13 +11,13 @@ main()
 	do
 		bat=$(cat /sys/class/power_supply/BAT0/capacity)
 		if [[ $bat -le 50 && $bat -gt 20 ]]; then
-			notify-send "Battery low warning, Battery level is ${bat}%"
+			notify-send "Battery Warning" "Warning: Battery level is ${bat}%"
 
 		elif [[ $bat -le 20 && $bat -gt 5 ]]; then
-			notify-send -u critical "Battery very low warning Battery level is ${bat}%"
+			notify-send -u critical "Battery Warning" "Low: Battery level is ${bat}%"
 
 		elif [[ $bat -le 5 ]]; then
-			notify-send -u critical "Battery critical, please shutdown Battery level is ${bat}%"
+			notify-send -u critical "Battery Warning" "Critial: Battery level is ${bat}%"
 
 		fi
 		sleep 10m
