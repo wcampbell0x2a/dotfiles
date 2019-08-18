48c5df5dc5b041230734738946f121a1b0160b0b
userWayneCampbell <wcampbell1995@gmail.com>
2019-08-18 16:06:57 -0400

    Revert "Add notify all output of udevadmmonitor"
    This reverts commit c6332453e4c778f191c23d942d83ffc6812fe3f1.

--- scripts/scripts/udevadmmonitor.sh
+++ scripts/scripts/udevadmmonitor.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ##
 # Auto mount usb devices
-#
+# 
 # found on: https://wiki.archlinux.org/index.php/udisks
 ##
 
@@ -12,6 +12,6 @@ pathtoname() {
 stdbuf -oL -- udevadm monitor --udev -s block | while read -r -- _ _ event devpath _; do
         if [ "$event" = add ]; then
             devname=$(pathtoname "$devpath")
-            udisksctl mount --block-device "$devname" --no-user-interaction |& while read OUTPUT; do notify-send "udisksctl" "$OUTPUT"; done
+            udisksctl mount --block-device "$devname" --no-user-interaction | while read OUTPUT; do notify-send "udisksctl" "$OUTPUT"; done
         fi
 done
