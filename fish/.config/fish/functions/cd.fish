c9427016ce001f4e498c4cbfd8b9609f52ed06ef
wcampbell <wcampbell1995@gmail.com>
2023-09-08 17:35:29 -0400

    Replace exa with eza

--- fish/.config/fish/functions/cd.fish
+++ fish/.config/fish/functions/cd.fish
@@ -2,9 +2,9 @@
 function cd --description "auto ls for each cd"
   if [ -n $argv[1] ]
     builtin cd $argv[1]
-    and exa -la
+    and eza -la
   else
     builtin cd ~
-    and exa -la
+    and eza -la
   end
 end
