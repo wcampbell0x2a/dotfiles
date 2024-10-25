3fd033195d6572147139c65452dc95e08c885edf
wcampbell <wcampbell1995@gmail.com>
2024-10-24 23:59:23 -0400

    fish: Remove unused wifi* functions
    Thank god I use iw now

--- fish/.config/fish/functions/wifilist.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function wifilist --description 'alias wifilist nmcli device wifi rescan; nmcli device wifi list'
-	nmcli device wifi rescan; nmcli device wifi list $argv;
-end
--- fish/.config/fish/functions/wifion.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function wifion --description 'alias wifion nmcli radio wifi on'
-	nmcli radio wifi on $argv;
-end
